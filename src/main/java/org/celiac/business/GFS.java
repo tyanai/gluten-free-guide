c3cf0c35004cf8393137d2a605453e352a480d3e
tyanai <tyanai@192.168.1.185>
2017-03-30 23:15:06 +0300

    Update

--- src/main/java/org/celiac/business/GFS.java
+++ src/main/java/org/celiac/business/GFS.java
@@ -5,7 +5,7 @@ import java.util.SortedSet;
 
 public interface GFS {
 
-	String search(String smsText) throws Exception;
+	String search(String smsText) throws  Exception;
 	String searchMULTIPLE(String smsText) throws Exception;
 	Map<String,SortedSet<String>> searchForAllBasedOnCategory(String aCompany, String aCategory) throws Exception ;
 	
